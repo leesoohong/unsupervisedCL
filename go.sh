7b40a2be633a046381b7814de6c884d7aa7a1f80
dltnghd0407@gmail.com <dltnghd0407@gmail.com>
2021-12-28 12:34:05 +0900

    1228

---
+++ go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+for seed in 0 1 2 3 4 
+do
+  python consis_clr.py --DS $1 --lr 0.001 --local --num-gc-layers 3 --aug $2 --alpha $3 --seed $seed
+  python consis_clr.py --DS $1 --lr 0.01 --local --num-gc-layers 3 --aug $2 --alpha $3 --seed $seed
+
+done
+
---
+++ path.py
@@ -0,0 +1,5 @@
+import os
+import os.path as osp
+
+path = osp.join(osp.dirname(osp.realpath(__file__)) ,'.', 'data','NCI1')
+print(path)
